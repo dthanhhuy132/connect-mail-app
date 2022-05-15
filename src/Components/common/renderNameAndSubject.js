fd19ca08aef76200515353edaa3cb7b2c75be7de
huydoan <dthanhhuy132@gmail.com>
2022-05-16 00:03:42 +0700

    modify header mail

--- src/Components/common/renderNameAndSubject.js
+++ src/Components/common/renderNameAndSubject.js
@@ -12,12 +12,12 @@ const renderNameAndSubject = (mailId, isMailIdValid) => {
     return (
       <span className="text-gray-500 flex items-center gap-2">
         <span>{mailTarget.senderName.first + ' ' + mailTarget.senderName.last}</span> |
-        <span className="text-gray-500 font-[300] w-[250px] overflow-hidden text-ellipsis whitespace-nowrap inline-block">
-          {mailTarget.subject}
-        </span>
+        <span className="text-gray-500 font-[300] ">{mailTarget.subject}</span>
       </span>
     );
   }
 };
+
+// w-[250px] overflow-hidden text-ellipsis whitespace-nowrap inline-block
 
 export default renderNameAndSubject;
